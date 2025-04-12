44294943e2a1f34f7dd16dd4e0c8d735d11f9ac8
Jason Dang <65359022+jasondang91@users.noreply.github.com>
2025-04-13 02:04:12 +0700

    update for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import gzipPlugin from 'rollup-plugin-gzip'
 import dotenv from 'dotenv'
 dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	base: '/portfolio-me',
-	build: {
-		outDir: 'dist',
-		assetsDir: 'assets',
-		rollupOptions: {
-			output: {
-				manualChunks: undefined,
-			},
-			plugins: [
-				gzipPlugin({
-					filter: /\.(js|css|html|json|svg)$/,
-					minSize: 1024,
-				}),
-			],
-		},
-	},
 })
